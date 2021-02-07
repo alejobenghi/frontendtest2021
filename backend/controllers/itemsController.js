58c11ee521ed83396f74de73ff786974e3945af7
alejobenghi <alejo.bgh@gmail.com>
2021-02-07 10:04:38 -0300

    commit 2

---
+++ backend/controllers/itemsController.js
@@ -0,0 +1,65 @@
+//const { request } = require("express")
+const request = require('request');
+async function getItems(req,res){
+    try {
+        var url = `https://api.mercadolibre.com/sites/MLA/search?q=:${req.query.q}`
+        request(url, function(error,response,body){
+            var data=JSON.parse(body)
+            
+
+            var categories = []
+            if(data.results.length >=4){
+                dim = 4
+            }else{
+                dim = data.results.length
+            }
+            for(var i=0;i<dim;i++){
+                if(!categories.includes(data.results[i].category_id)){
+                    categories.push(data.results[i].category_id)
+                }
+            }
+            
+
+            var json = '{"author":{"name":"Alejo","lastname":"Benedetti Ghiglia"},'
+            json +='   "categories":['
+            if(categories.length > 0){
+                categories.forEach(element => {
+                    json +='"'+element +'",'
+                })
+                json = json.slice(0,-1)
+            }
+            
+            json +='],'
+            json +='"items":['
+            for(var i=0;i<dim;i++){
+                json +='{'
+                json += '"id":'+'"'+data.results[i].id+'",'
+                json += '"title":'+'"'+data.results[i].title+'",'
+                json += '"price":{'
+                json += '"currency":"'+data.results[i].prices.prices[0].currency_id+'",'
+                json += '"amount":'+data.results[i].prices.prices[0].amount+','
+                json += '"decimals":2'
+                json +='},'
+                json +='"picture":"",'
+                json +='"condition":"",'
+                json +='"free_shipping":'+data.results[i].shipping.free_shipping
+                json +='},'
+            }
+            if(dim>=1){
+                json = json.slice(0,-1)
+            }
+            json +=']'
+            json +='}'
+            res.status(200).send(JSON.parse(json))
+            //res.status(200).send(data)
+
+        })
+        
+    } catch (e) {
+        res.status(500).send({message:e.message})
+    }
+}
+
+module.exports = {
+    getItems
+}
--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const { getItems } = require('../controllers/itemsController')
 const api = express.Router()
 
-api.post('/items',(req,res) => res.status(201).send({success: true}))
+//api.post('/items',(req,res) => res.status(201).send({success: true}))
+api.get('/items/',getItems)
 
 module.exports = api
